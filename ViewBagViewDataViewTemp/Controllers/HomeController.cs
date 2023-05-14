9ed0828709e0f7e1366612fb40cbe2ad3a76067d
ZumaMamedova <zumrudam@code.edu.az>
2023-05-14 12:41:19 +0400

    ViewBag/ViewData/TempData

---
+++ ViewBagViewDataViewTemp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ViewBagViewDataViewTemp.Models;
+
+namespace ViewBagViewDataViewTemp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            //ViewData
+
+            var p = new Product
+            {
+                Id = 1,
+                Name = "Product1",
+                Price = 122
+            };
+
+            //ViewData["ProductName"] = p.Name;
+            //ViewData["Name"] = "Name";
+            //ViewData["Surname"] = "Surname";    
+            //ViewData["Age"] = 20;
+
+            //ViewBag
+
+
+            //TempData
+            ViewData["Name"] = "Name";
+            ViewBag.Surname = "Surname";
+            TempData["Age"] = 25;
+
+
+            return RedirectToAction("About");
+        }
+
+        public IActionResult About() 
+        {
+            return View();
+        }
+    }
+}
